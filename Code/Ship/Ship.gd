55fcb00c341ec102758a92b899f2d772d0f92e07
unblinky <47279470+unblinky@users.noreply.github.com>
2023-10-26 19:49:52 -0700

    Ship has art. Flying sideways.

--- Code/Ship/Ship.gd
+++ Code/Ship/Ship.gd
@@ -1,24 +1,36 @@
-extends Area2D
+extends ScreenWrapper
 class_name Ship
 
 const BULLET = preload("res://Bullet/Bullet.tscn")
 
-var rotation_speed: float = 180 # Degrees / sec.
+var thrust_power: float = 10.0 #??
+var rotation_speed: float = 180 # ° / sec.
+var velocity: Vector2 # px / sec.
 
 
 func _process(delta):
+	super._process(delta)
+	
 	if Input.is_action_pressed("turn_ccw"):
 		print("CCW")
 		rotation_degrees -= rotation_speed * delta
 	if Input.is_action_pressed("turn_cw"):
 		print("CW")
 		rotation_degrees += rotation_speed * delta
+	
+	# Thrust.
 	if Input.is_action_pressed("thrust"):
 		print("Thrust")
+		var direction = Vector2(cos(rotation), -sin(rotation))
+		direction.y *= -1
+		velocity += direction * thrust_power
+	
+	# Fire.
 	if Input.is_action_just_pressed("fire"):
 		print("Fire")
 		var bullet = BULLET.instantiate()
 		bullet.position = position
 		bullet.rotation = rotation
 		get_parent().add_child(bullet)
-
+	
+	position += velocity * delta
