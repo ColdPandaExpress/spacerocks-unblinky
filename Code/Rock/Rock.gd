4dced7b1d164a646f790dc7491454e0361c75bc7
unblinky <47279470+unblinky@users.noreply.github.com>
2023-10-26 19:11:26 -0700

    ScreenWrapper started. And renamed image files.

--- Code/Rock/Rock.gd
+++ Code/Rock/Rock.gd
@@ -1,18 +1,20 @@
-extends Area2D
+extends ScreenWrapper
 class_name Rock
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 var speed: float = randf_range(70, 350)
 var direction: Vector2 = Vector2(randf_range(-1, 1), randf_range(-1, 1))
-#var direction: Vector2 = Vector2(1, 0)
+var rotation_speed: float = randf_range(-180, 180) # angle.
+
+
+func _ready():
+	animated_sprite_2d.frame = randi_range(0, animated_sprite_2d.sprite_frames.get_frame_count("default") - 1)
+
 
 func _process(delta):
-	position += direction * speed * delta
+	super._process(delta) # Handle wrapping.
 	
-	if position.x < 0:
-		position.x = get_viewport().size.x
-	if position.x > get_viewport().size.x:
-		position.x = 0
-	if position.y < 0:
-		position.y = get_viewport().size.y
-	if position.y > get_viewport().size.y:
-		position.y = 0
+	position += direction * speed * delta
+	rotation_degrees += rotation_speed * delta
+	
---
+++ Code/ScreenWrapper/ScreenWrapper.gd
@@ -0,0 +1,15 @@
+extends Node2D
+class_name ScreenWrapper
+
+
+func _process(_delta):
+	
+	# Wrapping.
+	if position.x < 0:
+		position.x = get_viewport().size.x
+	if position.x > get_viewport().size.x:
+		position.x = 0
+	if position.y < 0:
+		position.y = get_viewport().size.y
+	if position.y > get_viewport().size.y:
+		position.y = 0
